df63d2d071f5fd7e018b1b161b8e67bddbb545ae
ekinuc <ekinucc2004@gmail.com>
2024-02-09 12:15:58 +0300

    get_next_line

---
+++ get_next_line.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: euc <euc@student.42istanbul.com.tr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/28 16:36:23 by euc               #+#    #+#             */
+/*   Updated: 2024/01/28 16:36:23 by euc              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 4
+# endif
+
+# include <unistd.h>
+# include <stdlib.h>
+
+size_t	ft_strlen(const char *s);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strjoin(char *s1, char *s2);
+char	*get_main_line(char *putline);
+char	*get_last_main(char *putline);
+char	*get_read_line(int fd, char *putline);
+char	*get_next_line(int fd);
+
+#endif
